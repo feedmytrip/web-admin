fe03caa7299e602c564e3f2b5a65a6c719dcdc6e
André Luiz Mendonça <andreluzz@gmail.com>
2018-12-11 21:01:03 -0200

    Sort timeline events on the same render type group

--- src/components/common/timeline/TimelineEvent.vue
+++ src/components/common/timeline/TimelineEvent.vue
@@ -18,11 +18,27 @@
         class="timeline-drag has-background-info is-pulled-left"
         id="offset"
       />
+      <div class="timeline-sort is-pulled-left">
+        <i
+          v-if="sort && sortup && (draggable == 'offset' || draggable == 'all')"
+          class="fa fa-arrow-up timeline-sort-icon"
+          aria-hidden="true"
+          @click="sortUp"
+        ></i>
+      </div>
       <div
         v-if="draggable == 'duration' || draggable == 'all'"
         class="timeline-drag has-background-info is-pulled-right"
         id="duration"
       />
+      <div class="timeline-sort is-pulled-right">
+        <i
+          v-if="sort && sortdown && (draggable == 'duration' || draggable == 'all')"
+          @click="sortDown"
+          class="fa fa-arrow-down timeline-sort-icon"
+          aria-hidden="true"
+        ></i>
+      </div>
       <div class="has-background-white is-capitalized timeline-event is-clearfix">
         {{event.title.pt }}
       </div>
@@ -32,7 +48,7 @@
 
 <script>
 export default {
-  props: ['event', 'renderType', 'offset', 'duration', 'draggable'],
+  props: ['event', 'renderType', 'offset', 'duration', 'draggable', 'sort', 'sortup', 'sortdown'],
   computed: {
     moving () {
       return this.$store.getters['trips/moving']
@@ -51,14 +67,28 @@ export default {
     }
   },
   methods: {
+    sortUp () {
+      const payload = {
+        type: this.renderType,
+        event: this.event
+      }
+      this.$emit('sort-up', payload)
+    },
+    sortDown () {
+      const payload = {
+        type: this.renderType,
+        event: this.event
+      }
+      this.$emit('sort-down', payload)
+    },
     setDragHandler: function (e) {
-      console.log(e.target.id)
       this.handler = e.target.id
     },
     dragstart: function (e, type) {
       if (this.handler === 'duration' || this.handler === 'offset') {
         e.target.style.opacity = 0.1
         const payload = {
+          type: this.renderType,
           event: this.event,
           dragging: this.handler
         }
@@ -81,6 +111,13 @@ export default {
   height: 20px;
   border: 1px solid black;
   cursor: move;
+}
+.timeline-sort {
+  width: 15px;
+  height: 20px;
+}
+.timeline-sort-icon {
+  cursor: pointer;
 }
 .timeline-event {
   height: 20px;
--- src/components/common/timeline/TimelineEventsList.vue
+++ src/components/common/timeline/TimelineEventsList.vue
@@ -2,13 +2,18 @@
   <div style="position: absolute; top: 105px; width: 100%;">
     <template v-for="render in renderEvents">
       <fmt-timeline-event
-        v-for="value in render.events"
+        v-for="(value, index) in render.events"
         :key="value.id"
         :event="value"
         :render-type="render.type"
         :offset="render.offset"
         :duration="render.duration"
         :draggable="render.draggable"
+        :sort="render.events.length > 1 ? true : false"
+        :sortup="index > 0 ? true : false"
+        :sortdown="index < render.events.length - 1 && render.sort ? true : false"
+        v-on:sort-up="sortUp"
+        v-on:sort-down="sortDown"
       ></fmt-timeline-event>
     </template>
   </div>
@@ -20,6 +25,66 @@ export default {
   props: ['events', 'day'],
   components: {
     'fmt-timeline-event': TimelineEvent
+  },
+  methods: {
+    async sortUp (payload) {
+      const loading = this.$loading.open()
+      const index = this.$_.findIndex(this.renderEvents, { type: payload.type })
+      const renderElement = this.renderEvents[index]
+      const typeEvents = renderElement.events
+      const eventIindex = this.$_.findIndex(typeEvents, { id: payload.event.id })
+      for (let i = 0; i < typeEvents.length; i++) {
+        let offset = renderElement.period_offset + i
+        let duration = renderElement.period_duration - i
+        if (i === eventIindex) {
+          offset = renderElement.period_offset + (i - 1)
+          duration = renderElement.period_duration - (i - 1)
+        } else if (i === eventIindex - 1) {
+          offset = renderElement.period_offset + (i + 1)
+          duration = renderElement.period_duration - (i + 1)
+        }
+        await this.updateEvent(typeEvents[i].id, typeEvents[i].itinerary_id, offset, duration)
+      }
+      loading.close()
+    },
+    async sortDown (payload) {
+      const loading = this.$loading.open()
+      const index = this.$_.findIndex(this.renderEvents, { type: payload.type })
+      const renderElement = this.renderEvents[index]
+      const typeEvents = renderElement.events
+      const eventIindex = this.$_.findIndex(typeEvents, { id: payload.event.id })
+      for (let i = 0; i < typeEvents.length; i++) {
+        let offset = renderElement.period_offset + i
+        let duration = renderElement.period_duration - i
+        if (i === eventIindex) {
+          offset = renderElement.period_offset + (i + 1)
+          duration = renderElement.period_duration - (i + 1)
+        } else if (i === eventIindex + 1) {
+          offset = renderElement.period_offset + (i - 1)
+          duration = renderElement.period_duration - (i - 1)
+        }
+        await this.updateEvent(typeEvents[i].id, typeEvents[i].itinerary_id, offset, duration)
+      }
+      loading.close()
+    },
+    async updateEvent (id, itineraryId, offset, duration) {
+      await (new Promise((resolve, reject) => {
+        const payload = {
+          trip_id: this.$route.params.id,
+          itinerary_id: itineraryId,
+          id: id,
+          begin_offset: offset,
+          duration: duration
+        }
+        this.$store.dispatch('trips/updateItineraryEvent', payload)
+          .then(() => {
+            resolve()
+          })
+          .catch(err => {
+            reject(err)
+          })
+      }))
+    }
   },
   computed: {
     renderEvents () {
@@ -36,6 +101,7 @@ export default {
           draggable: 'none',
           offset: 0,
           duration: 100,
+          sort: false,
           events: this.$_.filter(events, function (e) {
             return e.begin_offset < dayMorIni && e.begin_offset + e.duration > dayNgtEnd
           })
@@ -45,6 +111,7 @@ export default {
           draggable: 'offset',
           offset: 0,
           duration: 100,
+          sort: false,
           events: this.$_.filter(events, function (e) {
             return (e.begin_offset >= dayMorIni && e.begin_offset < dayMorEnd) && e.begin_offset + e.duration > dayNgtEnd
           })
@@ -54,6 +121,7 @@ export default {
           draggable: 'duration',
           offset: 0,
           duration: 100,
+          sort: false,
           events: this.$_.filter(events, function (e) {
             return e.begin_offset < dayMorIni && (e.begin_offset + e.duration > dayNgtIni && e.begin_offset + e.duration <= dayNgtEnd)
           })
@@ -63,6 +131,9 @@ export default {
           draggable: 'all',
           offset: 0,
           duration: 100,
+          sort: true,
+          period_offset: dayMorIni,
+          period_duration: 86400,
           events: this.$_.filter(events, function (e) {
             return (e.begin_offset >= dayMorIni && e.begin_offset < dayMorEnd) && (e.begin_offset + e.duration > dayNgtIni && e.begin_offset + e.duration <= dayNgtEnd)
           })
@@ -72,6 +143,7 @@ export default {
           draggable: 'duration',
           offset: 0,
           duration: 32,
+          sort: false,
           events: this.$_.filter(events, function (e) {
             return e.begin_offset < dayMorIni && (e.begin_offset + e.duration > dayMorIni && e.begin_offset + e.duration <= dayMorEnd)
           })
@@ -81,6 +153,9 @@ export default {
           draggable: 'all',
           offset: 0,
           duration: 32,
+          sort: true,
+          period_offset: dayMorIni,
+          period_duration: 21600,
           events: this.$_.filter(events, function (e) {
             return e.begin_offset >= dayMorIni && e.begin_offset + e.duration <= dayMorEnd
           })
@@ -90,6 +165,9 @@ export default {
           draggable: 'all',
           offset: 0,
           duration: 66,
+          sort: true,
+          period_offset: dayMorIni,
+          period_duration: 43200,
           events: this.$_.filter(events, function (e) {
             return (e.begin_offset >= dayMorIni && e.begin_offset < dayMorEnd) && (e.begin_offset + e.duration > dayAftIni && e.begin_offset + e.duration <= dayAftEnd)
           })
@@ -98,7 +176,9 @@ export default {
           type: 'T08', // Não começa no dia mas termina no dia a tarde
           draggable: 'duration',
           offset: 0,
+          sort: false,
           duration: 66,
+          sort: false,
           events: this.$_.filter(events, function (e) {
             return e.begin_offset < dayMorIni && (e.begin_offset + e.duration > dayAftIni && e.begin_offset + e.duration <= dayAftEnd)
           })
@@ -108,6 +188,9 @@ export default {
           draggable: 'all',
           offset: 34,
           duration: 32,
+          sort: true,
+          period_offset: dayAftIni,
+          period_duration: 21600,
           events: this.$_.filter(events, function (e) {
             return e.begin_offset >= dayAftIni && e.begin_offset + e.duration <= dayAftEnd
           })
@@ -117,6 +200,9 @@ export default {
           draggable: 'all',
           offset: 34,
           duration: 66,
+          sort: true,
+          period_offset: dayAftIni,
+          period_duration: 64800,
           events: this.$_.filter(events, function (e) {
             return (e.begin_offset >= dayAftIni && e.begin_offset < dayAftEnd) && (e.begin_offset + e.duration > dayAftEnd && e.begin_offset + e.duration <= dayNgtEnd)
           })
@@ -126,6 +212,7 @@ export default {
           draggable: 'offset',
           offset: 34,
           duration: 66,
+          sort: false,
           events: this.$_.filter(events, function (e) {
             return (e.begin_offset >= dayAftIni && e.begin_offset < dayAftEnd) && e.begin_offset + e.duration > dayNgtEnd
           })
@@ -135,6 +222,9 @@ export default {
           draggable: 'all',
           offset: 68,
           duration: 32,
+          sort: true,
+          period_offset: dayNgtIni,
+          period_duration: 43200,
           events: this.$_.filter(events, function (e) {
             return e.begin_offset >= dayNgtIni && e.begin_offset + e.duration < dayNgtEnd
           })
@@ -144,6 +234,7 @@ export default {
           draggable: 'offset',
           offset: 68,
           duration: 32,
+          sort: false,
           events: this.$_.filter(events, function (e) {
             return (e.begin_offset >= dayNgtIni && e.begin_offset < dayNgtEnd) && e.begin_offset + e.duration >= dayNgtEnd
           })
